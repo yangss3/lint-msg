884135c277a4719776190b067ffb66c68dfad60f
yangss3 <yss_2016@outlook.com>
2022-03-22 20:28:00 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import path from 'path'
+import fs from 'fs-extra'
+import chalk from 'chalk'
+const msgPath = path.resolve(process.cwd(), '.git/COMMIT_EDITMSG')
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
+const releaseRegEx = /^v?(0|[1-9]\d*)(\.(0|[1-9]\d*))*$/
+const msgRegEx = /^(revert: )?(feat|fix|docs|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps|style)(\(.+\))?: .{1,50}/
+if (!releaseRegEx.test(msg) && !msgRegEx.test(msg)) {
+  console.log()
+  console.error(`${chalk.red.bold(' Error: ')}${chalk.redBright('invalid commit message format.')}
+  See ${chalk.cyanBright('https://www.conventionalcommits.org')} for more details.
+`)
+  process.exit(1)
+}
