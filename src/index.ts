ce9a088d518f567942a3f62c861fcaed9b237f7e
yangss3 <yss_2016@outlook.com>
2022-03-23 19:38:26 +0800

    feat: use picocolors

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@
 
 import path from 'path'
 import fs from 'fs-extra'
-import chalk from 'chalk'
+import color from 'picocolors'
+
 const msgPath = path.resolve(process.cwd(), '.git/COMMIT_EDITMSG')
 const msg = fs.readFileSync(msgPath, 'utf-8').trim()
 const releaseRegEx = /^v?(0|[1-9]\d*)(\.(0|[1-9]\d*))*$/
 const msgRegEx = /^(revert: )?(feat|fix|docs|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps|style)(\(.+\))?: .{1,50}/
+
 if (!releaseRegEx.test(msg) && !msgRegEx.test(msg)) {
   console.log()
-  console.error(`${chalk.red.bold(' Error: ')}${chalk.redBright('invalid commit message format.')}
-  See ${chalk.cyanBright('https://www.conventionalcommits.org')} for more details.
+  console.error(`${color.bgRed(color.bold(color.white(' Error: ')))}${color.red('invalid commit message format.')}
+  See ${color.cyan('https://www.conventionalcommits.org')} for more details.
 `)
   process.exit(1)
 }
+
